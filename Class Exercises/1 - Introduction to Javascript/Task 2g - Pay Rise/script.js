7d4d1bcc3626ebdf0a8922e239d151dd10eb188f
Alex-wqq <628ye39@canonslade.tbft.uk>
2026-09-11 09:38:09 +0000

    Lesson

--- Class Exercises/1 - Introduction to Javascript/Task 2g - Pay Rise/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2g - Pay Rise/script.js
@@ -4,8 +4,16 @@ document.getElementById('calculateBtn').addEventListener('click', calculatePayRi
 function calculatePayRise() {
     // TODO:
     // 1. Get the current salary, months back dated, and pay rise percentage
+    let salary = parseFloat(document.getElementById('currentSalary').value);
+    let months = parseFloat(document.getElementById('months').value);
+    let percentage = parseFloat(document.getElementById('percentage').value);
     // 2. Calculate:
     //    - New annual salary (current salary + percentage increase)
+    const increase = salary * percentage;
+    const newSalary = salary + increase;
     //    - Back pay (difference in monthly salary × number of months)
+    const difference = newSalary * months;
     // 3. Display both results formatted to 2 decimal places
+    document.getElementById('newSalary').textContent = newSalary;
+    document.getElementById('backPay').textContent = difference;
 }
