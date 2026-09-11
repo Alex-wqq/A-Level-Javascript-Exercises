3b078446638d928dab0752de1f4de930a758c717
Alex-wqq <628ye39@canonslade.tbft.uk>
2026-09-11 09:44:02 +0000

    Lesson

--- Class Exercises/1 - Introduction to Javascript/Task 2h - Overtime/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2h - Overtime/script.js
@@ -5,12 +5,22 @@ function calculatePay() {
     // TODO:
     // 1. Get all input values:
     //    - Standard hourly rate
+    let hours = parseFloat(document.getElementById('standardRate').value);
     //    - Overtime rate
+    let overtime = parseFloat(document.getElementById('overtimeRate').value);
     //    - Basic hours worked
+    let basic = parseFloat(document.getElementById('basicHours').value);
     //    - Overtime hours worked
+    let overtimeWorked = parseFloat(document.getElementById('overtimeHours').value);
     // 2. Calculate:
     //    - Basic pay (standard rate × basic hours)
+    const basicpay = hours * basic;
     //    - Overtime pay (overtime rate × overtime hours)
+    const overtimePay = overtime * overtimeWorked;
     //    - Total pay (basic pay + overtime pay)
+    const total = basicpay + overtimePay;
     // 3. Display all amounts formatted to 2 decimal places
+    document.getElementById('basicPay').textContent = basicpay.toFixed(2)
+    document.getElementById('overtimePay').textContent = overtimePay.toFixed(2)
+    document.getElementById('totalPay').textContent = total.toFixed(2)
 }
--- Class Exercises/1 - Introduction to Javascript/Task 3b - Color Changer/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 3b - Color Changer/script.js
@@ -6,12 +6,13 @@ document.getElementById('boxBorder').addEventListener('input', updateColors);
 
 function updateColors() {
     // TODO:
-    // 1. Get all color input values
+    // 1. Get all colour input values
+    let 
     // 2. Update the styles of each element:
-    //    - Heading text color
+    //    - Heading text colour
     //    - Paragraph background
     //    - Box background
-    //    - Box border color
+    //    - Box border colour
     // 3. Update the color value displays with the current hex codes
     
     // Hint: To change styles use element.style.property = value
