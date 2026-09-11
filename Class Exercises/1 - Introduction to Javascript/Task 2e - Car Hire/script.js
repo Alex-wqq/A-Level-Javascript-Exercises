8dc91b2bf119ad2f81eeb77693688da1723daedc
Alex-wqq <628ye39@canonslade.tbft.uk>
2026-09-11 09:28:21 +0000

    Lesson

--- Class Exercises/1 - Introduction to Javascript/Task 2e - Car Hire/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2e - Car Hire/script.js
@@ -4,8 +4,13 @@ document.getElementById('calculateBtn').addEventListener('click', calculateHireC
 function calculateHireCharge() {
     // TODO:
     // 1. Get the miles travelled from the input field
+    let miles = parseFloat(document.getElementById('miles').value);
     // 2. Calculate:
     //    - Mileage charge (5p per mile = £0.05 per mile)
+    const mileage = miles * 1.05;
     //    - Total charge (£25.00 + mileage charge)
+    const total = 25 + mileage
     // 3. Display both charges formatted to 2 decimal places
+    document.getElementById('mileageCharge').textContent = mileage.toFixed(2)
+    document.getElementById('totalCharge').textContent = total.toFixed(2)
 }
--- Class Exercises/1 - Introduction to Javascript/Task 2f - Commission/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2f - Commission/script.js
@@ -4,12 +4,22 @@ document.getElementById('calculateBtn').addEventListener('click', calculateCommi
 function calculateCommission() {
     // TODO:
     // 1. Get all invoice numbers and amounts from the input fields
+    let invoice1 = parseFloat(document.getElementById('invoice1').value);
+    let invoice2 = parseFloat(document.getElementById('invoice2').value);
+    let invoice3 = parseFloat(document.getElementById('invoice3').value);
     // 2. Calculate:
     //    - Total sales amount
+    const totalSale = invoice1 + invoice2 + invoice3
     //    - Commission (20% of total sales)
+    const commission = totalSale * 0.20
     // 3. Display:
     //    - Each sale's details
+    document.getElementById('amount1').textContent = invoice1.toFixed(2)
+    document.getElementById('amount2').textContent = invoice2.toFixed(2)
+    document.getElementById('amount3').textContent = invoice3.toFixed(2)
     //    - Total sales amount
+    document.getElementById('totalSales').textContent = totalSale.toFixed(2)
     //    - Commission earned
+    document.getElementById('commission').textContent = commission.toFixed(2)
     // Note: Format all monetary values to 2 decimal places
 }
