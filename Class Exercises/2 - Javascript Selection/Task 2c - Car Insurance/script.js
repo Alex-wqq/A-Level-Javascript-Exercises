e699e98ae1dcd489a8cdc314afe9626103816460
Alex-wqq <628ye39@canonslade.tbft.uk>
2026-09-14 11:56:33 +0000

    Lesson

--- Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
@@ -10,16 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the insurance premium
 function calculatePremium() {
     // TODO: Get values from input fields
-    
+    let age = parseFloat(document.getElementById('age').value);
+    let noClaims = parseFloat(document.getElementById('noClaims').value);
+    let previousClaims = parseFloat(document.getElementById('previousClaims').value);
     // TODO: Calculate base premium (£500)
-    
+    const basePremium = 500;
     // TODO: Apply age adjustment
     // Under 25: +50%
     // 25-40: No change
     // Over 40: -15%
-    
+    if (age < 25) {
+        let ageAdjustment = (500/2) + 500;
+    } else if (age >= 25 && age <= 40) {
+        let ageAdjustment = 500;
+    } else {
+        let ageAdjustment = 500 - 75;
+    }
     // TODO: Apply no claims bonus
     // 0 years: No discount
+    if (noClaims = 0) {
+        let bonus = ageAdjustment
+    }
     // 1-2 years: 10% discount
     // 3-5 years: 25% discount
     // Over 5 years: 35% discount
