21dfa12840a8681d8624a69ad975081e336f303e
Alex-wqq <628ye39@canonslade.tbft.uk>
2026-09-16 09:50:07 +0000

    Lesson

--- Class Exercises/2 - Javascript Selection/Task 2d - Movie Tickets/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2d - Movie Tickets/script.js
@@ -10,16 +10,53 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the total ticket cost
 function calculateTotal() {
     // TODO: Get values from all input fields
-    let adultTickets = parseFloat(document.getElementById('adultTickets').value)
-    let childTickets = parseFloat(document.getElementById('childTickets').value)
-    let seniorTickets = parseFloat(document.getElementById('seniorTickets').value)
-    let price = 0;
+    let adultTickets = parseFloat(document.getElementById('adultTickets').value);
+    let childTickets = parseFloat(document.getElementById('childTickets').value);
+    let seniorTickets = parseFloat(document.getElementById('seniorTickets').value);
     // TODO: Calculate base costs
     if (adultTickets >= 1) {
-        let priceAdult = 12.00 * adultTickets 
-    } else if (childTickets >= 1)
-        let priceChild = 8.00 * childTickets
+        var priceAdult = 12.00 * adultTickets ;
+    } else if (childTickets >= 1) {
+        var priceChild = 8.00 * childTickets;
+    } else {
+        var priceSenior = 7.50 * seniorTickets;
     }
+    
+    var price = priceAdult + priceChild + priceSenior;
+    let totalTickets = adultTickets + childTickets + seniorTickets
+
+    let day = document.getElementById('dayOfWeek').value;
+    if (day == 'Friday' || day == 'Saturday' || day == 'Sunday'){
+        price.value = 2.50 * price;
+    } else {
+        price.value = price;
+    }
+
+    let time = document.getElementById('showingTime');
+    if (time < '17:00') {
+        price.value = price - (totalTickets * 1.50);
+    }
+
+    let subtotal = price
+
+    if (adultTickets == 2 && childTickets == 2) {
+        price.value = price * 0.90
+        var discount = '10%'
+        document.getElementById('priceBreakdown').textContent = `${subtotal} * ${discount}`
+        document.getElementById('subtotal').textContent = subtotal
+        document.getElementById('discount').textContent = discount
+        document.getElementById('finalTotal').textContent = price
+
+    } else if (totalTickets >= 6) {
+        price.value = price * 0.85
+        discount = '15%'
+        document.getElementById('priceBreakdown').textContent = `${subtotal} * ${discount}`
+        document.getElementById('subtotal').textContent = subtotal
+        document.getElementById('discount').textContent = discount
+        document.getElementById('finalTotal').textContent = price
+
+    }
+
     // Adult: £12.00
     // Child: £8.00
     // Senior: £7.50
